7b23f993da9d853946b8c5449a6a69b0cedcbe35
Chun-Hung,WSL,pro6 <johnbamboobilly@DESKTOP-8J948KJ.localdomain>
2019-10-19 11:04:02 +0800

    First

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Header from '../Header';
-import MemeGenerator from '../MemeGenerator';
+import YouTube from 'react-youtube';
+//import Webcam from "react-webcam";
+//import MemeGenerator from '../MemeGenerator';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<Header />
-				<MemeGenerator />
+				<YouTube
+					videoId="0EyaTqezSzs"
+					onReady={this._onReady}
+				/>
 			</div>
 		);
 	}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,11 @@
 //Header
-//only display thing, so make it a functional component
 import React from 'react';
 
 function Header() {
 	return (
 		<header>
-			<img 
-				src = 'http://www.pngall.com/wp-content/uploads/2016/07/Arrow-Free-Download-PNG.png'
-				alt= 'Hey'
-			/>
-			<p>Welcome to meme generator</p>
+			<h1>Hello navigator :)</h1>
+			<p>Welcome to Garbage Patch in Pacific ocean!!!</p>
 		</header>
 	);
 }
